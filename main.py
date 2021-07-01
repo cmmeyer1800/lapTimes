c303ec75152278c6f2d2dc6e2f5bf7d262f9a267
collin <collinmmeyer@gmail.com>
2021-06-30 20:07:56 -0500

    added get index, added api to post to lap time database

--- main.py
+++ main.py
@@ -16,8 +16,29 @@ def send_to_db(time):
     else:
         return 0
 
+def get_from_db():
+    client = bigquery.Client.from_service_account_json(r"/home/collin/code/python/lapTimes/testing-315021-0d231ce1adcd.json")
+    query = "SELECT * FROM `testing-315021.lapTimes.testing` LIMIT 100"
+    result = client.query(query)
+    data = []
+    for x in result:
+        row = []
+        for y in x:
+            row.append(y)
+        data.append(row)
+    return data
 
+@app.route('/api/submit/time', methods=['POST'])
+def submit_data():
+    data = request.json
+    lap_time = data["time"]
+    result = send_to_db(lap_time)
+    if result == 0:
+        return 'success'
+    elif result == -1:
+        return 'An error has occured inserting into the database'
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    data = get_from_db()
+    return render_template('index.html', data=data)
