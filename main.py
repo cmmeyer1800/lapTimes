9927b2b919b700933b15552a120e09a584ea04c3
collin <collinmmeyer@gmail.com>
2021-07-21 21:44:59 -0500

    fully working flask socketio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, jsonify
 from flask_caching import Cache
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, send
 from google.cloud import bigquery
 from datetime import datetime
 import json
@@ -17,6 +17,9 @@ class Datastore():
         self.data = []
         self.current_time = ''
 
+    def __iter__(self):
+        return (x for x in self.data)
+
     def send_to_bq(self):
         formattedData = [{'json':self.data}]
         result = self.client.insert_rows_json(self.table_id, formattedData)
@@ -30,40 +33,47 @@ class Datastore():
         time = datetime.now().strftime("%m-%d-%y %H:%M:%S")
         self.data.append({'datetime':time, 'laptime':data})
 
+    def formatted_data(self):
+        output = []
+        for idx in range(1, len(self.data)):
+            output.append({'date':self.data[idx]['datetime'][0:8], 'lap_num':idx, 'lap_time':self.data[idx]['laptime']-self.data[idx-1]['laptime']})
+            if idx > 1:
+                output[idx-1]['difference'] = output[idx-1]['lap_time']-output[idx-2]['lap_time']
+            else:
+                output[idx-1]['difference'] = 0
+        return output
+
 db = Datastore()
-
-def formatData(data):
-    output = []
-    for idx in range(1, len(data)):
-        output.append({'date':data[idx]['datetime'][0:8], 'lap_num':idx, 'lap_time':data[idx]['laptime']-data[idx-1]['laptime']})
-        if idx > 1:
-            output[idx-1]['difference'] = output[idx-1]['lap_time']-output[idx-2]['lap_time']
-        else:
-            output[idx-1]['difference'] = 0
-    return output
     
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    data = formatData(db.data)
-    return render_template('index.html', data=data)
+    return redirect('live')
+
+@app.route('/live', methods=['GET', 'POST'])
+def live():
+    if request.method == 'GET':
+        return render_template('live.html')
+    elif request.method == 'POST':
+        pass
+        # Process POST
+    else:
+        return 'Method is not allowed'
+
+@app.route('/history/<id>', methods=['GET'])
+def history(id):
+    return str(id)
 
 @app.route('/api/submit/time', methods=['POST'])
 def add_data():
     time_data = request.get_json()['time']
     db.add_data(time_data)
-    socketio.emit('message', 'NEW DATA')
+    if len(db.formatted_data()) > 0:
+        socketio.emit('add_data', json.dumps([db.formatted_data()[-1]]), json=True)
     return 'success'
 
-@app.route('/api/reset', methods=['POST'])
-def submit_collection():
-    db.send_to_bq()
-    db.data = []
-    return 'success'
-
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data['data'])
+@socketio.on('connect')
+def connect():
+    emit('add_data', json.dumps(db.formatted_data()), json=True)
 
 if __name__ == "__main__":
     socketio.run(app)
--- static/live.js
+++ static/live.js
@@ -1,16 +1,31 @@
+current_data = []
+var gtable;
+
 $(document).ready( function() {
     var table = $('#lapTable').DataTable({
         searchPanes: true
     });
+    gtable = table;
 });
 
 var socket = io();
+
 socket.on('connect', function() {
-    socket.emit('message', {data: 'I\'m connected!'});
+    console.log('connection established');
 });
-socket.on('message', function(data) {
-    console.log(data);
+
+socket.on('add_data', function(lap_data) {
+    current_data = JSON.parse(lap_data);
+    for(idx = 0; idx < current_data.length; idx = idx + 1){
+        gtable.row.add([
+            current_data[idx].date,
+            current_data[idx].lap_num,
+            (current_data[idx].lap_time/1000).toString()+' Seconds',
+            (current_data[idx].difference/1000).toString()+' Seconds'
+        ]).draw();
+    }
 });
+
 /*
 function updateTable(){
     $.getJSON("_times",
