b5255089aeb058941a41c17ad63e0b46d1fa8236
collin <collinmmeyer@gmail.com>
2021-08-16 00:38:15 -0500

    fully working login system

---
+++ lapapp/main.py
@@ -0,0 +1,18 @@
+from flask import render_template, Blueprint
+from flask_login import login_required
+from . import db
+
+main = Blueprint('main', __name__)
+
+@main.route('/history', methods=['GET'])
+def history():
+    return render_template('history.html')
+
+@main.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@main.route('/docs', methods=['GET'])
+@login_required
+def docs():
+    return render_template('docs.html')
