74cb3bc6f40350be77e082d00b9458347f5f1309
collin <collinmmeyer@gmail.com>
2021-08-16 14:10:28 -0500

    procfile added

--- lapapp/models.py
+++ lapapp/models.py
@@ -6,3 +6,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
