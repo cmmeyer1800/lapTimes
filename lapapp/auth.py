6c690fef33a8c5de37e3f528aeb017d982162174
collin <collinmmeyer@gmail.com>
2021-08-18 21:17:48 -0500

    removed signup because it doesn't makes sense for our use case

--- lapapp/auth.py
+++ lapapp/auth.py
@@ -27,28 +27,6 @@ def login_post():
         return redirect(url_for(f'main.{next[1:]}'))
     return redirect(url_for('main.index'))
 
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
-
-@auth.route('/signup', methods=['POST'])
-def signup_post():
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-
-    user = User.query.filter_by(name=name).first() # if this returns a user, then the email already exists in database
-
-    if user: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
-
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('auth.login'))
-
 @auth.route('/logout')
 @login_required
 def logout():
